89646534322a198cfd9640a3539df3df73e61519
Jeff Triplett <jeff.triplett@gmail.com>
2015-02-15 15:04:43 -0600

    Initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Quick and simple app to render a Markdown document in a webpage and to provide reasonable styles for printing.
+
+### Themes used:
+- https://sphinx-readable-theme.readthedocs.org/en/latest/_static/readable.css
+- https://raw.githubusercontent.com/furtivecss/print/master/lib/print.css
---
+++ web.py
@@ -0,0 +1,52 @@
+import markdown
+import os
+import requests
+import requests_cache
+
+from bs4 import BeautifulSoup
+from flask import Flask, Markup, render_template, request
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+requests_cache.install_cache('demo_cache', expire_after=1800)
+
+
+def find_title(soup):
+    # Determine the page title from the first heading
+    title = soup.find(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+    if title:
+        return title.string
+    return None
+
+
+@app.route('/')
+def homepage():
+    url = request.args.get('url')
+    clear = request.args.get('clear', False)
+
+    if clear:
+        requests_cache.clear()
+
+    if url:
+        req = requests.get(url)
+        content = req.text
+
+        if content:
+            content = Markup(markdown.markdown(content))
+            soup = BeautifulSoup(content)
+            title = find_title(soup)
+            context = {
+                'title': title,
+                'content': content,
+            }
+            return render_template('index.html', **context)
+
+    return 'I should make a homepage. For now append ?url= to the page which contains markdown.'
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
